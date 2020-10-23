2a9d3dfd3c95dddc3edbf037291b298e4cf6f913
Mike Karamousadakis <mickaram@hotmail.com>
2020-10-23 15:25:15 +0300

    rename repo

---
+++ anybotics_any_node/include/anybotics_any_node/Param.hpp
@@ -0,0 +1,16 @@
+/*!
+ * @file	Param.hpp
+ * @author	Philipp Leemann
+ * @date	July, 2016
+ */
+
+#pragma once
+
+#include "anybotics_param_io/get_param.hpp"
+#include "anybotics_param_io/set_param.hpp"
+
+namespace any_node {
+
+using namespace param_io;  // NOLINT
+
+}  // namespace any_node
